100221fc495b19c1a40ccd40203798d0cc429038
XBT3K <43539643+XBT3K@users.noreply.github.com>
2023-02-18 15:18:05 +0000

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-def mean_reversion(indicator, long_threshold=-1.0, short_threshold=1.0):
+def mean_reversion(indicator, long_threshold=-1.0, short_threshold=1.0, stop_loss=2.0, take_profit=2.0):
     signals = pd.DataFrame(index=indicator.index)
-    signals['
+    signals['Positions'] = 0
+    signals['Buy'] = (indicator['Indicator'] < long_threshold).astype(int)
+    signals['Sell'] = (indicator['Indicator'] > short_threshold).astype(int)
+    signals['Stop Loss'] = indicator['Lower Band'] * stop_loss
+    signals['Take Profit'] = indicator['Upper Band'] * take_profit
+    return signals
